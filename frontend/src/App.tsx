1164f8f4cd612808fc2a3f400333be576a5f7cbf
kyonakyona0615 <kyonakyona0615@gmail.com>
2024-12-15 17:00:50 +0900

    fix イミュータブルに保つ

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -14,7 +14,6 @@ function App() {
   const [todos, setTodos] = useState<Todo[]>([]);
 
   const handleSubmit = () => {
-
     if (!text) return;
 
     const newTodo: Todo = {
@@ -24,30 +23,49 @@ function App() {
 
     setTodos((todos) => [newTodo, ...todos]);
 
-    setText('');
+    setText("");
   };
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setText(e.target.value);
   };
 
+  const handleEdit = (id: number, value: string) => {
+    setTodos((todos) => {
+      const newTodos = todos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, value: value };
+        }
+        return todo;
+      });
+
+      return newTodos;
+    });
+  };
+
   return (
     <div>
-      <form onSubmit={(e) => {
+      <form
+        onSubmit={(e) => {
           e.preventDefault();
           handleSubmit();
-        }}>
+        }}
+      >
         <input type="text" value={text} onChange={(e) => handleChange(e)} />
-        <input
-          type="submit"
-          value="追加"
-          onSubmit={handleSubmit}
-        />
+        <input type="submit" value="追加" onSubmit={handleSubmit} />
       </form>
 
       <ul>
         {todos.map((todo) => {
-          return <li key={todo.id}>{todo.value}</li>;
+          return (
+            <li key={todo.id}>
+              <input
+                type="text"
+                value={todo.value}
+                onChange={(e) => handleEdit(todo.id, e.target.value)}
+              />
+            </li>
+          );
         })}
       </ul>
     </div>
